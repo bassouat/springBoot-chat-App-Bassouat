8ddd92967dec2a672f1de123ae98ad52a321c89a
bassouat <bassouat8@gmail.com>
2021-03-28 15:07:21 +0200

    SpringBootApplicationAppBasseydou

---
+++ src/main/java/com/bassouat/springBootchatAppBassouat/SpringBootChatAppBassouatApplication.java
@@ -0,0 +1,13 @@
+package com.bassouat.springBootchatAppBassouat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppBassouatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppBassouatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bassouat/springBootchatAppBassouat/SpringBootChatAppBassouatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bassouat.springBootchatAppBassouat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatAppBassouatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
